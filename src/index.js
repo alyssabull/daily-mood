479962a664c31cf59f4b490f9a2c410cdfcd2f27
Alyssa Bull <alyssabull@gmail.com>
2021-03-09 08:56:09 -0700

    Set up auth0 in index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
 import { BrowserRouter } from 'react-router-dom'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Auth0Provider
+    domain='dev-vimwdwcg.us.auth0.com'
+    clientId='rNAfttdUarv3sA8P5nQAvFixtwDZDBQ6'
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Auth0Provider>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
