7549b4cd04d06a512801c4a3abd4bd21baac671f
Alyssa Bull <alyssabull@gmail.com>
2021-03-08 20:30:01 -0700

    Make square background

--- src/App/App.js
+++ src/App/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from '../Home/Home'
 import './App.scss';
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,34 @@
+import React, { useState } from 'react'
 import './Home.scss'
 
 const Home = () => {
+  const [browserSize, updateBrowserSize] = useState({height: Math.ceil(document.documentElement.clientHeight/60), width: Math.ceil(document.documentElement.clientWidth/60)})
+  
+  const boxContainerStyles = {
+    display: 'grid',
+    gridTemplateColumns: `repeat(${browserSize.width}, 60px)`,
+    position: 'absolute'
+  }
+
+  const generateBoxes = () => {
+    let boxNum = browserSize.height * browserSize.width
+    let boxes = []
+    for (let i = 0; i < boxNum; i++) {
+      boxes.push(<div className={`box ${i}`}></div>)
+    }
+    return boxes
+  }
+
   return(
     <div className='home-container'>
-      <h1>Daily Mood</h1>
-      <button>ENTER</button>
+    <div className='box-container' style={ boxContainerStyles }>
+      {generateBoxes()}
     </div>
+    <div className='title-container'>
+      <h1 className='title'>Daily Mood</h1>
+      <button className='enter-button'>ENTER</button>
+    </div>
+  </div>
   )
 }
 
